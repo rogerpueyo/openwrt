607663f9ba5db1e1d0ceeda71caf75831f0d1b0b
Roger Pueyo Centelles <roger.pueyo@guifi.net>
2020-01-26 17:56:05 +0100

    ath79: add support for MikroTik RouterBOARD 922UAGS-5HPacD [WiP]
    This patch ports support for the MikroTik RouterBOARD 922UAGS-5HPacD
    with a built-in 802.11ac High-Power radio (31dBm), which was already
    available in the ar71xx target.
    
    See https://mikrotik.com/product/RB922UAGS-5HPacD for more info.
    
    Specifications:
    - SoC: Qualcomm Atheros QCA9558 (720 MHz)
    - RAM: 128 MB
    - Storage: 128 MB NAND
    - Wireless: external QCA9882 802.11a/ac 2x2:2
    - Ethernet: 1x 1000/100/10 Mbps, integrated, via AR8031 PHY, passive PoE-in 24V
    - SFP: 1x host
    - USB: 1x 2.0 type A
    - PCIe: 1x Mini slot (also contains USB 2.0 for 3G/LTE modems)
    - SIM slot: 1x mini-SIM
    
    Working:
    - Board/system detection
    - SPI and NAND storage
    - PCIe
    - USB type A host
    - Wireless
    - Ethernet
    - LEDs (user, phy0)
    - Reset button
    - Sysupgrade to/from ar71xx
    
    Not supported:
    - RSSI LEDs
    - SFP cage
    
    Installation methods:
    - Sysupgrade from ar71xx (it is advisable to use the -n option to
    wipe any previous settings), or
    - Boot the initramfs image via TFTP and then flash the sysupgrade
    image using "sysupgrade -n"
    
    Signed-off-by: Roger Pueyo Centelles <roger.pueyo@guifi.net>

--- target/linux/ath79/nand/base-files/lib/upgrade/platform.sh
+++ target/linux/ath79/nand/base-files/lib/upgrade/platform.sh
@@ -7,13 +7,36 @@ platform_check_image() {
 	return 0
 }
 
-RAMFS_COPY_BIN='fw_printenv fw_setenv'
+RAMFS_COPY_BIN='fw_printenv fw_setenv nandwrite'
 RAMFS_COPY_DATA='/etc/fw_env.config /var/lock/fw_printenv.lock'
+
+platform_do_upgrade_mikrotik_rb() {
+	CI_KERNPART=none
+
+	local fw_mtd=$(find_mtd_part kernel)
+	fw_mtd="${fw_mtd/block/}"
+	[ -n "$fw_mtd" ] || return
+
+	mtd erase kernel
+
+	if $(tar xf "$1" sysupgrade-${2/,/_}/kernel -O > /dev/null 2>&1); then
+		# ath79 sysupgrade images with per-device board name
+		tar xf "$1" sysupgrade-${2/,/_}/kernel -O | nandwrite -o "$fw_mtd" -
+	elif $(tar xf "$1" sysupgrade-routerboard/kernel -O > /dev/null 2>&1); then
+		# downgrade to ar71xx sysupgrade images with generic "routerboard" board name
+		tar xf "$1" sysupgrade-routerboard/kernel -O | nandwrite -o "$fw_mtd" -
+	fi
+
+	nand_do_upgrade "$1"
+}
 
 platform_do_upgrade() {
 	local board=$(board_name)
 
 	case "$board" in
+	mikrotik,routerboard-922uags-5hpacd)
+		platform_do_upgrade_mikrotik_rb "$1"
+		;;
 	glinet,gl-ar300m-nand|\
 	glinet,gl-ar300m-nor)
 		glinet_nand_nor_do_upgrade "$1"

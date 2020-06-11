1eb1b091e05d712b82aaf263d2a67a100b1a6308
Roger Pueyo Centelles <roger.pueyo@guifi.net>
2020-06-11 12:56:25 +0200

    ath79: add support for MikroTik LHG XL 2 (RBLHG-2nD-XL)
    The MikroTik LHG XL 2 (RBLHG-2nD-XL) is a 2.4 GHz 802.11b/g/n outdoor
    device with a feed and an integrated dual polarization grid dish
    antenna based on the LHG-HB platform.
    
    See https://mikrotik.com/product/lhg_xl_2 for more info.
    
    Specifications:
     - SoC: Qualcomm Atheros QCA9533
     - RAM: 64 MB
     - Storage: 16 MB NOR
     - Wireless: Atheros AR9531 (SoC) 802.11b/g/n 2x2:2, 21 dBi antenna
     - Ethernet: Atheros AR8229 (SoC), 1x 10/100 port, 12-28 Vdc PoE in
     - 8 user-controllable LEDs:
       · 1x power (blue)
       · 1x user (green)
       · 1x lan (green)
       · 1x wlan (green)
       · 4x rssi (green)
    
    Note:
     The rssihigh LED is disabled, as it shares GPIO 16 with the reset
     button.
    
    Flashing:
     TFTP boot initramfs image and then perform sysupgrade. Follow common
     MikroTik procedure as in https://openwrt.org/toh/mikrotik/common.
    
    Signed-off-by: Roger Pueyo Centelles <roger.pueyo@guifi.net>

--- target/linux/ath79/image/mikrotik.mk
+++ target/linux/ath79/image/mikrotik.mk
@@ -29,6 +29,8 @@ define Device/mikrotik_lhg-hb
   DEVICE_MODEL := LHG-HB platform
   DEVICE_ALT0_VENDOR := MikroTik
   DEVICE_ALT0_MODEL := LHG 2 (RBLHG-2nD)
+  DEVICE_ALT1_VENDOR := MikroTik
+  DEVICE_ALT1_MODEL := LHG XL 2 (RBLHG-2nD-XL)
   IMAGE_SIZE := 16256k
   IMAGE/sysupgrade.bin := append-kernel | kernel2minor -s 1024 -e | \
 	pad-to $$$$(BLOCKSIZE) | append-rootfs | pad-rootfs | \

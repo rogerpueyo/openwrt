750d4a8627f97e01f60e53dc68e17cf9e799a95e
Roger Pueyo Centelles <roger.pueyo@guifi.net>
2020-10-23 16:05:28 +0200

    ipq40xx: add support for MikroTik SXTsq 5 ac
    This commit adds support for the MikroTik SXTsq 5 ac (RBSXTsqG-5acD),
    an outdoor 802.11ac wireless CPE with one 10/100/1000 Mbps Ethernet
    port.
    
    Specifications:
     - SoC: Qualcomm Atheros IPQ4018
     - RAM: 256 MB
     - Storage: 16 MB NOR
     - Wireless: IPQ4018 (SoC) 802.11a/n/ac 2x2:2, 16 dBi antennae
     - Ethernet: IPQ4018 (SoC) 1x 10/100/1000 port, 10-28 Vdc PoE in
     - 1x Ethernet LED (green)
     - 7x user-controllable LEDs
      · 1x power (blue)
      · 1x user (green)
      · 5x rssi (green)
    
    Note:
     Serial UART is probably available on the board, but it has not been
     tested.
    
    Flashing:
     Boot via TFTP the initramfs image. Then, upload a sysupgrade image
     via SSH and flash it normally. More info at the "Common procedures
     for MikroTik products" page https://openwrt.org/toh/mikrotik/common.
    
    Signed-off-by: Roger Pueyo Centelles <roger.pueyo@guifi.net>

--- target/linux/ipq40xx/image/mikrotik.mk
+++ target/linux/ipq40xx/image/mikrotik.mk
@@ -16,3 +16,11 @@ define Device/mikrotik_hap-ac2
 	DEVICE_PACKAGES := ipq-wifi-mikrotik_hap-ac2 -kmod-ath10k-ct kmod-ath10k-ct-smallbuffers
 endef
 TARGET_DEVICES += mikrotik_hap-ac2
+
+define Device/mikrotik_sxtsq-5-ac
+	$(call Device/MikrotikNOR)
+	DEVICE_MODEL := SXTsq 5 ac (RBSXTsqG-5acD)
+	SOC := qcom-ipq4018
+	DEVICE_PACKAGES := rssileds ipq-wifi-mikrotik_sxtsq-5-ac
+endef
+TARGET_DEVICES += mikrotik_sxtsq-5-ac
